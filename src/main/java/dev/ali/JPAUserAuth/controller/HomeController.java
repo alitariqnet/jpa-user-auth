a3001df279021aa7b268a59dd66b529901970a0f
Ali Tariq <alibabapk@aol.com>
2024-05-31 02:55:37 +0500

    Retrieve user information in Spring security

--- src/main/java/dev/ali/JPAUserAuth/controller/HomeController.java
+++ src/main/java/dev/ali/JPAUserAuth/controller/HomeController.java
@@ -1,8 +1,14 @@
 package dev.ali.JPAUserAuth.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @RestController
 public class HomeController {
@@ -23,5 +29,25 @@ public class HomeController {
     public String helloAdmin(){
         return "Hello admin!";
     }
-
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @GetMapping(value = "/unamewithprincipal")
+    public String currentUserName(Principal principal) {
+        return principal.getName();
+    }
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @GetMapping(value = "/unamewithauth")
+    public String currentUserName(Authentication authentication) {
+        return authentication.getName();
+    }
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @GetMapping(value = "/unamewithservletreq")
+    public String currentUserNameSimple(HttpServletRequest request) {
+        Principal principal = request.getUserPrincipal();
+        return principal.getName();
+    }
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @GetMapping("/userdetails")
+    public String getUser(@AuthenticationPrincipal UserDetails userDetails) {
+        return "User Details: " + userDetails.getUsername();
+    }
 }
